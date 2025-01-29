35b6e3bb98a0831c67da2b411c738a6f33c3ab79
Henry Chen <henrychen385@gmail.com>
2025-01-29 11:31:19 +0800

    feat: Add free space path loss and signal quantization functions to channel module

--- common/channel.py
+++ common/channel.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def free_space_path_loss(signal, distance_m, sig_freq_hz):
+    """
+    Calculates the free space path loss. based on the friis equation.
+    """
+    speed_of_light = 3e8
+    wavelength = speed_of_light / sig_freq_hz
+    return signal / (4 * np.pi * distance_m / wavelength)**2
+
 def awgn_channel(signal, snr_db):
     """
     Adds AWGN to the signal.
@@ -14,3 +22,18 @@ def add_delay(signal, delay):
     Adds a delay to the signal.
     """
     return np.concatenate([np.zeros(delay), signal])
+
+def quantize_signal(signal, adc_bit_depth, amp_gain_db):
+    """
+    Quantizes the signal based on the ADC bit depth.
+    """
+    quantization_levels = 2**adc_bit_depth
+    sig_amp = signal * 10**(amp_gain_db/20)
+
+    sig_power = np.mean(np.abs(sig_amp)**2)
+
+    sig_real = np.round(np.real(sig_amp) * quantization_levels) / quantization_levels
+    sig_imag = np.round(np.imag(sig_amp) * quantization_levels) / quantization_levels
+    signal_quantized = sig_real + 1j * sig_imag
+
+    return signal_quantized
